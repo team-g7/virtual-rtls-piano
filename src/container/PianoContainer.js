bc272bc9b9ea2b802a1d0b37de4391948266af95
Mairesu <marusnak@online.no>
2019-03-21 19:11:32 +0100

    added position circle

--- src/container/PianoContainer.js
+++ src/container/PianoContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Tangent from "../components/Tangent";
 import MQTTConnection from '../config/MQTTConnection';
 import '../css/PianoContainer.css';
+import Konva from 'konva';
+import {Circle, Layer, Stage} from "react-konva";
 
 class PianoContainer extends React.Component {
 
@@ -35,35 +36,17 @@ class PianoContainer extends React.Component {
     render() {
         return (
           <div>
-              <canvas id="visualization-canvas" width="1200" height="600">
-                <div className="lab-background">
-
-                </div>
-              </canvas>
-
-
-
-
-
-
-
-
-
-              <Tangent note="C" xstart="5200" xend="5866" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="D" xstart="5866" xend="6532" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="E" xstart="6532" xend="7198" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="F" xstart="7198" xend="7198" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="G" xstart="7864" xend="8530" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="A" xstart="8530" xend="9196" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="H" xstart="9196" xend="9862" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="C" xstart="9862" xend="10528" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="D" xstart="10528" xend="11194" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="E" xstart="11194" xend="11860" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="F" xstart="11860" xend="12526" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="G" xstart="12526" xend="13192" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="A" xstart="13192" xend="13858" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="H" xstart="13858" xend="14525" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
-              <Tangent note="C" xstart="14525" xend="15190" ystart={this.state.yStartPos} yend={this.state.yEndPos} sound="sound_url"/>
+              <Stage width={window.innerWidth} height={window.innerHeight}>
+                  <Layer>
+                      <Circle
+                          x={0}
+                          y={0}
+                          radius={15}
+                          fill="#70ee00"
+                          shadowBlur={5}
+                      />
+                  </Layer>
+              </Stage>
           </div>
         );
     }
