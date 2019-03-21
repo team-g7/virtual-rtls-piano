b5a44e262e3780beeabc99dc69f154536051e193
Unknown <aronmar@live.no>
2019-03-21 20:46:24 +0100

    Fix static styling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import PianoContainer from './container/PianoContainer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<PianoContainer />, document.getElementById('pianoContainer'));
-ReactDOM.render(<App />, document.getElementById('pianoContainer'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
