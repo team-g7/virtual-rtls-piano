167c705a22f2e7eb8adba72b7c3106cd2994ccae
ANicholasson <aronmar@live.no>
2019-03-21 12:47:48 +0100

    tut code

---
+++ src/components/Tangent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ''
+
+class Tangent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            starting_x_pos: 2,
+            ending_x_pos : 0
+        }
+    }
+
+    handleSubmit() {
+
+    }
+
+    render() {
+        return (
+          <div className="red">
+              <p>
+                  asdasd
+              </p>
+          </div>
+        );
+    }
+
+}
+
+export default Tangent;
